1cbf5f0fde2f20163b9a2b9adb4a82d179d935b4
vbloodi <bloodi.code@gmail.com>
2023-01-20 21:37:40 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  const sortedConfig = bracketsConfig.map((item) => item.join(""));
+
+  while (str) {
+    for (i = 0; i < sortedConfig.length; i++) {
+      str = str.replaceAll(sortedConfig[i], "");
+    }
+    if (str && sortedConfig.every((item) => !str.includes(item))) {
+      return false;
+    }
+  }
+
+  return !str;
+};
