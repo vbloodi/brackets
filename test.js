ba6dd43660bb0d3cbb7627ead146e0dc121f582f
vbloodi <bloodi.code@gmail.com>
2023-05-19 20:13:06 +0300

    feat: add test.js

--- test.js
+++ test.js
@@ -2,12 +2,33 @@ const assert = require('assert');
 const check = require('./src/index.js');
 
 const config1 = [['(', ')']];
-const config2 = [['(', ')'], ['[', ']']];
-const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+const config2 = [
+  ['(', ')'],
+  ['[', ']'],
+];
+const config3 = [
+  ['(', ')'],
+  ['[', ']'],
+  ['{', '}'],
+];
 const config4 = [['|', '|']];
-const config5 = [['(', ')'], ['|', '|']];
-const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
-const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+const config5 = [
+  ['(', ')'],
+  ['|', '|'],
+];
+const config6 = [
+  ['1', '2'],
+  ['3', '4'],
+  ['5', '6'],
+  ['7', '7'],
+  ['8', '8'],
+];
+const config7 = [
+  ['(', ')'],
+  ['[', ']'],
+  ['{', '}'],
+  ['|', '|'],
+];
 
 it('should check if brackets sequence is correct 1', () => {
   assert.equal(check('()', config1), true);
